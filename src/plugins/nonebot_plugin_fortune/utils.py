32eb302441e753258bb3f239f1f69b86e16b4265
zhulinyv <zhulinyv2005@outlook.com>
2024-04-09 17:59:52 +0800

    construction: 修改位置

--- src/plugins/nonebot_plugin_fortune/utils.py
+++ src/plugins/nonebot_plugin_fortune/utils.py
@@ -72,8 +72,8 @@ def drawing(gid: str, uid: str, theme: str, spec_path: Optional[str] = None) -> 
     font_length = ttfront.getbbox(title)
     draw.text(
         (
-            image_font_center[0] - font_length[0] / 2 - 70,
-            image_font_center[1] - font_length[1] / 2 - 20,
+            image_font_center[0] - (font_length[2] - font_length[0]) / 2,
+            image_font_center[1] - (font_length[3] - font_length[1]) / 2,
         ),
         title,
         fill=color,
