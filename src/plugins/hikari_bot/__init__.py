ffb7094ec0df8e1c65faeb00e024d8439ca8ac80
zhulinyv <zhulinyv2005@outlook.com>
2024-04-09 15:18:18 +0800

    :construction: 修改启动提示

--- src/plugins/hikari_bot/__init__.py
+++ src/plugins/hikari_bot/__init__.py
@@ -8,7 +8,6 @@ from collections import defaultdict, namedtuple
 
 import httpx
 from hikari_core import callback_hikari, init_hikari, set_hikari_config
-from hikari_core.data_source import __version__
 from hikari_core.game.help import check_version
 from hikari_core.model import Hikari_Model
 from hikari_core.moudle.wws_real_game import get_diff_ship
@@ -37,19 +36,21 @@ from .game.ocr import (
 from .game.pupu import get_pupu_msg
 from .utils import DailyNumberLimiter, FreqLimiter, download, get_bot
 
-scheduler = require('nonebot_plugin_apscheduler').scheduler
+scheduler = require("nonebot_plugin_apscheduler").scheduler
 
 _max = 100
-EXCEED_NOTICE = f'您今天已经冲过{_max}次了，请明早5点后再来！'
+EXCEED_NOTICE = f"您今天已经冲过{_max}次了，请明早5点后再来！"
 is_first_run = True
 _nlmt = DailyNumberLimiter(_max)
 _flmt = FreqLimiter(3)
-__bot_version__ = '1.0.6'
+__bot_version__ = "1.0.6"
 
-bot_get_random_pic = on_fullmatch('wws 随机表情包', block=True, priority=5)
-bot_update = on_fullmatch('wws 更新Hikari', priority=5, block=True, permission=SUPERUSER)
-bot = on_command('wws', block=False, aliases={'WWS'}, priority=54)
-bot_pupu = on_fullmatch('噗噗', block=False, priority=5)
+bot_get_random_pic = on_fullmatch("wws 随机表情包", block=True, priority=5)
+bot_update = on_fullmatch(
+    "wws 更新Hikari", priority=5, block=True, permission=SUPERUSER
+)
+bot = on_command("wws", block=False, aliases={"WWS"}, priority=54)
+bot_pupu = on_fullmatch("噗噗", block=False, priority=5)
 bot_listen = on_message(priority=5, block=False)
 ocr_listen = on_message(priority=6, block=False)
 driver = get_driver()
@@ -63,61 +64,78 @@ set_hikari_config(
     http2=driver.config.http2,
     proxy=_proxy,
     token=driver.config.api_token,
-    game_path=str(dir_path / 'game'),
+    game_path=str(dir_path / "game"),
 )
 
-SlectState = namedtuple('SlectState', ['state', 'SlectIndex', 'SelectList'])
+SlectState = namedtuple("SlectState", ["state", "SlectIndex", "SelectList"])
 SecletProcess = defaultdict(lambda: SlectState(False, None, None))
 
 
 @bot.handle()
-async def main(bot: Bot, ev: MessageEvent, matchmsg: Message = CommandArg()):  # noqa: B008, PLR0915
+async def main(
+    bot: Bot, ev: MessageEvent, matchmsg: Message = CommandArg()
+):  # noqa: B008, PLR0915
     try:
         server_type = None
-        if isinstance(ev, PrivateMessageEvent) and (driver.config.private or str(ev.user_id) in driver.config.superusers):  # 私聊事件,superusers默认不受影响
-            server_type = 'QQ'
-        elif isinstance(ev, GroupMessageEvent) and driver.config.group and ev.group_id not in driver.config.ban_group_list:  # 群聊事件
-            server_type = 'QQ'
+        if isinstance(ev, PrivateMessageEvent) and (
+            driver.config.private or str(ev.user_id) in driver.config.superusers
+        ):  # 私聊事件,superusers默认不受影响
+            server_type = "QQ"
+        elif (
+            isinstance(ev, GroupMessageEvent)
+            and driver.config.group
+            and ev.group_id not in driver.config.ban_group_list
+        ):  # 群聊事件
+            server_type = "QQ"
         elif isinstance(ev, GuildMessageEvent) and driver.config.channel:  # 频道事件
             if driver.config.all_channel or ev.channel_id in driver.config.channel_list:
-                server_type = 'QQ_CHANNEL'
+                server_type = "QQ_CHANNEL"
             else:
                 return False
         else:
             return False
         qqid = ev.user_id
         group_id = None
-        if ev.message_type == 'group':
+        if ev.message_type == "group":
             group_id = ev.group_id
         if not _nlmt.check(qqid):
             await bot.send(ev, EXCEED_NOTICE, at_sender=True)
             return False
         if not _flmt.check(qqid):
-            await bot.send(ev, '您冲得太快了，请稍候再冲', at_sender=True)
+            await bot.send(ev, "您冲得太快了，请稍候再冲", at_sender=True)
             return False
         _flmt.start_cd(qqid)
         _nlmt.increase(qqid)
-        superuser_command_list = ['重置监控']
-        adminuser_command_list = ['添加监控', '删除监控']
+        superuser_command_list = ["重置监控"]
+        adminuser_command_list = ["添加监控", "删除监控"]
         for each in superuser_command_list:
-            if (each in str(ev.message) or each in matchmsg) and str(qqid) not in driver.config.superusers:
-                await bot.send(ev, '该命令仅限超级管理员使用')
+            if (each in str(ev.message) or each in matchmsg) and str(
+                qqid
+            ) not in driver.config.superusers:
+                await bot.send(ev, "该命令仅限超级管理员使用")
                 return
         if str(qqid) not in driver.config.superusers:
             for each in adminuser_command_list:
-                if (each in str(ev.message) or each in matchmsg) and qqid not in driver.config.admin_list:
-                    await bot.send(ev, '请联系机器人搭建者添加权限')
+                if (
+                    each in str(ev.message) or each in matchmsg
+                ) and qqid not in driver.config.admin_list:
+                    await bot.send(ev, "请联系机器人搭建者添加权限")
                     return
-        hikari = await init_hikari(platform=server_type, PlatformId=str(qqid), command_text=str(matchmsg), GroupId=group_id)
-        if hikari.Status == 'success':
+        hikari = await init_hikari(
+            platform=server_type,
+            PlatformId=str(qqid),
+            command_text=str(matchmsg),
+            GroupId=group_id,
+        )
+        if hikari.Status == "success":
             if isinstance(hikari.Output.Data, bytes):
                 await bot.send(ev, MessageSegment.image(hikari.Output.Data))
             elif isinstance(hikari.Output.Data, str):
                 await bot.send(ev, hikari.Output.Data)
-        elif hikari.Status == 'wait':
+        elif hikari.Status == "wait":
             await bot.send(ev, MessageSegment.image(hikari.Output.Data))
             hikari = await wait_to_select(hikari)
-            if hikari.Status == 'error':
+            if hikari.Status == "error":
                 await bot.send(ev, str(hikari.Output.Data))
                 return
             hikari = await callback_hikari(hikari)
@@ -130,14 +148,16 @@ async def main(bot: Bot, ev: MessageEvent, matchmsg: Message = CommandArg()):  #
     except ActionFailed:
         logger.warning(traceback.format_exc())
         try:
-            await bot.send(ev, '发不出图片，可能被风控了QAQ')
+            await bot.send(ev, "发不出图片，可能被风控了QAQ")
             return True
         except Exception:
             pass
         return False
     except Exception:
         logger.error(traceback.format_exc())
-        await bot.send(ev, '呜呜呜发生了错误，可能是网络问题，如果过段时间不能恢复请联系麻麻哦~')
+        await bot.send(
+            ev, "呜呜呜发生了错误，可能是网络问题，如果过段时间不能恢复请联系麻麻哦~"
+        )
 
 
 @bot_listen.handle()
@@ -151,7 +171,7 @@ async def change_select_state(bot: Bot, ev: MessageEvent):
                 SecletProcess[qqid] = SecletProcess[qqid]._replace(state=False)
                 SecletProcess[qqid] = SecletProcess[qqid]._replace(SlectIndex=int(msg))
             else:
-                await bot.send(ev, '请选择列表中的序号哦~')
+                await bot.send(ev, "请选择列表中的序号哦~")
         return
     except Exception:
         logger.error(traceback.format_exc())
@@ -159,7 +179,9 @@ async def change_select_state(bot: Bot, ev: MessageEvent):
 
 
 async def wait_to_select(hikari):
-    SecletProcess[hikari.UserInfo.PlatformId] = SlectState(True, None, hikari.Input.Select_Data)
+    SecletProcess[hikari.UserInfo.PlatformId] = SlectState(
+        True, None, hikari.Input.Select_Data
+    )
     a = 0
     while a < 40 and not SecletProcess[hikari.UserInfo.PlatformId].SlectIndex:
         a += 1
@@ -170,7 +192,7 @@ async def wait_to_select(hikari):
         return hikari
     else:
         SecletProcess[hikari.UserInfo.PlatformId] = SlectState(False, None, None)
-        return hikari.error('已超时退出')
+        return hikari.error("已超时退出")
 
 
 @ocr_listen.handle()
@@ -178,18 +200,20 @@ async def OCR_listen(bot: Bot, ev: MessageEvent):
     try:
         if not driver.config.ocr_on:
             return
-        if not (str(ev.message).find('[CQ:image') + 1):  # 判断收到的信息是否为图片，不是就退出
+        if not (
+            str(ev.message).find("[CQ:image") + 1
+        ):  # 判断收到的信息是否为图片，不是就退出
             return
-        tencent_url = ''
+        tencent_url = ""
         for seg in ev.message:
-            if seg.type == 'image':
-                tencent_url = seg.data['url']
-                filename = str(seg.data['file']).replace('.image', '')
+            if seg.type == "image":
+                tencent_url = seg.data["url"]
+                filename = str(seg.data["file"]).replace(".image", "")
         ocr_text = await pic2txt_byOCR(tencent_url, filename)
         if ocr_text:
-            match = re.search(r'^(/?)wws(.*?)$', ocr_text)
+            match = re.search(r"^(/?)wws(.*?)$", ocr_text)
             if match:
-                searchtag = re.sub(r'^(/?)wws', '', ocr_text)  # 删除wws和/wws
+                searchtag = re.sub(r"^(/?)wws", "", ocr_text)  # 删除wws和/wws
                 is_send = await main(bot, ev, searchtag)
                 if is_send:
                     await upload_OcrResult(ocr_text, filename)
@@ -208,7 +232,9 @@ async def send_random_ocr_image(bot: Bot, ev: MessageEvent):
             await bot.send(ev, str(img))
     except Exception:
         logger.error(traceback.format_exc())
-        await bot.send(ev, '呜呜呜发生了错误，可能是网络问题，如果过段时间不能恢复请联系麻麻哦~')
+        await bot.send(
+            ev, "呜呜呜发生了错误，可能是网络问题，如果过段时间不能恢复请联系麻麻哦~"
+        )
         return
 
 
@@ -217,23 +243,32 @@ async def update_Hikari(ev: MessageEvent, bot: Bot):
     try:
         from nonebot_plugin_reboot import Reloader
 
-        await bot.send(ev, '正在更新Hikari，完成后将自动重启，如果没有回复您已上线的消息，请登录服务器查看')
-        if hasattr(driver.config, 'nb2_path'):
+        await bot.send(
+            ev,
+            "正在更新Hikari，完成后将自动重启，如果没有回复您已上线的消息，请登录服务器查看",
+        )
+        if hasattr(driver.config, "nb2_path"):
             # 并发fastgit会429，改为顺序请求
             for each in nb2_file:
-                await download(each['url'], f"{driver.config.nb2_path}\\{each['name']}")
+                await download(each["url"], f"{driver.config.nb2_path}\\{each['name']}")
                 await asyncio.sleep(0.5)
 
             # await asyncio.gather(
             #    *[download(each["url"], f"{driver.config.nb2_path}\{each['name']}") for each in nb2_file]
             # )
-        logger.info(f'当前解释器路径{sys.executable}')
-        os.system(f'{sys.executable} -m pip install hikari-bot -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade')
-        os.system(f'{sys.executable} -m pip install hikari-core -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade')
-        os.system(f'{sys.executable} -m pip install nonebot-plugin-gocqhttp -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade')
+        logger.info(f"当前解释器路径{sys.executable}")
+        os.system(
+            f"{sys.executable} -m pip install hikari-bot -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade"
+        )
+        os.system(
+            f"{sys.executable} -m pip install hikari-core -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade"
+        )
+        os.system(
+            f"{sys.executable} -m pip install nonebot-plugin-gocqhttp -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade"
+        )
         Reloader.reload(delay=1)
     except RuntimeError:
-        if str(platform.system()).lower() == 'linux':
+        if str(platform.system()).lower() == "linux":
             try:
                 import multiprocessing
 
@@ -242,18 +277,24 @@ async def update_Hikari(ev: MessageEvent, bot: Bot):
                 sys.stdout.flush()
                 # not compatible with cmdline with '\n'
                 os.execv(
-                    os.readlink('/proc/self/exe'),
-                    open('/proc/self/cmdline', 'rb').read().replace(b'\0', b'\n').decode().split('\n')[:-1],
+                    os.readlink("/proc/self/exe"),
+                    open("/proc/self/cmdline", "rb")
+                    .read()
+                    .replace(b"\0", b"\n")
+                    .decode()
+                    .split("\n")[:-1],
                 )
             except Exception:
                 logger.error(traceback.format_exc())
-                await bot.send(ev, '自动更新失败了QAQ，请登录服务器查看具体报错日志')
+                await bot.send(ev, "自动更新失败了QAQ，请登录服务器查看具体报错日志")
         else:
             logger.error(traceback.format_exc())
-            await bot.send(ev, '不支持nb run启动的方式更新哦，请使用python bot.py 启动Hikari')
+            await bot.send(
+                ev, "不支持nb run启动的方式更新哦，请使用python bot.py 启动Hikari"
+            )
     except Exception:
         logger.error(traceback.format_exc())
-        await bot.send(ev, '自动更新失败了QAQ，请登录服务器查看具体报错日志')
+        await bot.send(ev, "自动更新失败了QAQ，请登录服务器查看具体报错日志")
 
 
 @driver.on_startup
@@ -268,16 +309,18 @@ async def startup():
 
 @driver.on_bot_connect
 async def remind(bot: Bot):
-    superid = driver.config.superusers
+    """superid = driver.config.superusers
     await bot.get_login_info()
     for each in superid:
         await bot.send_private_msg(user_id=int(each), message=f'Hikari已上线，当前BOT版本{__bot_version__},内核版本{__version__}')
+    """
+    logger.success("脑积水启动完成")
 
 
 async def startup_download(url, name):
     async with httpx.AsyncClient() as client:
         resp = await client.get(url, timeout=20)
-        with open(template_path / name, 'wb+') as file:
+        with open(template_path / name, "wb+") as file:
             file.write(resp.content)
 
 
@@ -294,27 +337,34 @@ async def job_listen_battle():
     bot = get_bot()
     hikari = Hikari_Model()
     hikari = await get_diff_ship(hikari)
-    if hikari.Status == 'success':
+    if hikari.Status == "success":
         for _each in hikari.Output.Data:
-            await bot.send_group_msg(group_id=_each['group_id'], message=_each['msg'])
+            await bot.send_group_msg(group_id=_each["group_id"], message=_each["msg"])
 
 
-scheduler.add_job(job_chech_version, 'cron', hour=12)
-scheduler.add_job(startup, 'cron', hour=4)
-scheduler.add_job(downlod_OcrResult, 'interval', minutes=10)
-scheduler.add_job(job_listen_battle, 'interval', minutes=driver.config.battle_listen_time)
+scheduler.add_job(job_chech_version, "cron", hour=12)
+scheduler.add_job(startup, "cron", hour=4)
+scheduler.add_job(downlod_OcrResult, "interval", minutes=10)
+scheduler.add_job(
+    job_listen_battle, "interval", minutes=driver.config.battle_listen_time
+)
 
 
 @bot_pupu.handle()
 async def send_pupu_msg(ev: MessageEvent, bot: Bot):
     try:
-        if driver.config.pupu and isinstance(ev, GroupMessageEvent) and driver.config.group and ev.group_id not in driver.config.ban_group_list:
+        if (
+            driver.config.pupu
+            and isinstance(ev, GroupMessageEvent)
+            and driver.config.group
+            and ev.group_id not in driver.config.ban_group_list
+        ):
             msg = await get_pupu_msg()
             await bot.send(ev, msg)
     except ActionFailed:
         logger.warning(traceback.format_exc())
         try:
-            await bot.send(ev, '噗噗寄了>_<可能被风控了QAQ')
+            await bot.send(ev, "噗噗寄了>_<可能被风控了QAQ")
         except Exception:
             pass
         return
