7372e931c6eb3b25ebe8c6523c93e7071b478bd0
zhulinyv <zhulinyv2005@outlook.com>
2024-04-13 10:30:59 +0800

    :construction: 添加指令

--- src/plugins/nonebot_plugin_RealESRGAN/__init__.py
+++ src/plugins/nonebot_plugin_RealESRGAN/__init__.py
@@ -1,19 +1,19 @@
-from .utils import *
-from nonebot.adapters.onebot.v11 import Message, MessageEvent, MessageSegment, Bot, Event
-from nonebot.params import T_State, CommandArg
-from nonebot.plugin import on_command
-from nonebot.adapters.onebot.v11.helpers import HandleCancellation
 from json import loads
+
 from nonebot import get_driver
+from nonebot.adapters.onebot.v11 import Bot, Event, Message, MessageEvent, MessageSegment
+from nonebot.adapters.onebot.v11.helpers import HandleCancellation
+from nonebot.params import CommandArg, T_State
+from nonebot.plugin import on_command
+
+from .utils import *
 
 try:
     api = loads(get_driver().config.json())["realesrgan_api"]
 except:
-    api = 'https://akhaliq-real-esrgan.hf.space/api/predict/'
+    api = "https://akhaliq-real-esrgan.hf.space/api/predict/"
 
-real_esrgan = on_command(
-    "重建", aliases={"real-esrgan", "超分辨率重建", "esrgan", "real_esrgan"}, priority=30
-)
+real_esrgan = on_command("重建", aliases={"超分", "real-esrgan", "超分辨率重建", "esrgan", "real_esrgan"}, priority=30)
 
 
 @real_esrgan.handle()
@@ -23,12 +23,12 @@ async def real_esrgan_handle_first(
     state: T_State,
     args: Message = CommandArg(),
 ):
-    state['id'] = event.get_user_id()
+    state["id"] = event.get_user_id()
     for seg in args:
         if seg.type == "text":
             state["mode"] = seg.data["text"].strip()
         if seg.type == "image":
-            state['img'] = event.message['image']
+            state["img"] = event.message["image"]
             break
 
 
@@ -50,7 +50,7 @@ async def real_esrgan_handle_img(event: MessageEvent, state: T_State):
         await real_esrgan.finish(Message(f"[CQ:at,qq={state['id']}]不是图捏"))
     # 下面来处理图片
     try:
-        json_data = img_encode_to_json(img, state['mode'])  # 先获取图片并进行编码
+        json_data = img_encode_to_json(img, state["mode"])  # 先获取图片并进行编码
         if json_data is None:
             await real_esrgan.finish(Message(f"[CQ:at,qq={state['id']}]服务器无法接收到这张图捏,要不重试试试？"))
         result = await get_result(json_data, api=api)  # 然后进行超分辨率重建
@@ -58,7 +58,7 @@ async def real_esrgan_handle_img(event: MessageEvent, state: T_State):
             await real_esrgan.finish(Message(f"[CQ:at,qq={state['id']}]这张图没能被正确解析，可能网络连接失败或者是由于远程服务器免费额度耗尽，如果是后者建议联系管理员使用自建仓库(免费)"))
         img = img_decode_from_json(result)  # 获取重建后图片并进行解码发送
         await real_esrgan.finish(MessageSegment.image(img))
-    except Exception as e:
+    except Exception:
         ...
         # print('错误类型是', e.__class__.__name__)
         # print('错误明细是', e)
